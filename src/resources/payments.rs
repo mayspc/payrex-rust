8a6ec0cc5850591999f5629c2b46b293380952ad
Sheape <paulpare@protonmail.com>
2025-10-12 08:48:20 +0800

    feat(payments): add builder for `UpdatePayment`

--- src/resources/payments.rs
+++ src/resources/payments.rs
@@ -49,9 +49,8 @@ pub struct Payment {
     pub payment_intent_id: PaymentIntentId,
     pub status: PaymentStatus,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub customer: Option<Metadata>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub payment_method: Option<PaymentMethod>,
+    pub customer: Option<Metadata>, // TODO: Add Customer type
+    pub payment_method: PaymentMethodTypes,
     pub refunded: bool,
     pub created_at: Timestamp,
     pub updated_at: Timestamp,
@@ -69,11 +68,18 @@ pub struct Billing {
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Address {
     pub line1: String,
-    pub line2: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub line2: Option<String>,
     pub city: String,
     pub state: String,
     pub postal_code: String,
     pub country: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PaymentMethodTypes {
+    #[serde(rename = "type")]
+    pub _type: PaymentMethod,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
@@ -90,4 +96,21 @@ pub struct UpdatePayment {
     pub description: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub metadata: Option<Metadata>,
+}
+
+impl UpdatePayment {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn description(mut self, description: impl Into<String>) -> Self {
+        self.description = Some(description.into());
+        self
+    }
+
+    pub fn metadata(mut self, metadata: Metadata) -> Self {
+        self.metadata = Some(metadata);
+        self
+    }
 }
